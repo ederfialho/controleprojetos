54334da3a0d39b9d099bc092ca908a153200acf8
ederfialho <dzrmail@gmail.com>
2023-07-01 22:53:43 -0300

    Correção de script e arquivo de propriedades, após testes locais.

--- back-end/cdp/src/main/resources/db/migration/V000__criacao_de_tabelas_iniciais.sql
+++ back-end/cdp/src/main/resources/db/migration/V000__criacao_de_tabelas_iniciais.sql
@@ -11,22 +11,22 @@ CREATE TABLE tb_disciplina (
   PRIMARY KEY (id_disciplina)
 ) ENGINE=MyISAM;
 
-INSERT INTO TB_DISCIPLINA (nome_disciplina) VALUES ('COORDENACAO');
-INSERT INTO TB_DISCIPLINA (nome_disciplina) VALUES ('PLANEJAMENTO');
-INSERT INTO TB_DISCIPLINA (nome_disciplina) VALUES ('ARQUIVO');
-INSERT INTO TB_DISCIPLINA (nome_disciplina) VALUES ('PROCESSO');
-INSERT INTO TB_DISCIPLINA (nome_disciplina) VALUES ('SISTEMAS');
-INSERT INTO TB_DISCIPLINA (nome_disciplina) VALUES ('TUBULACAO');
-INSERT INTO TB_DISCIPLINA (nome_disciplina) VALUES ('MECANICA');
-INSERT INTO TB_DISCIPLINA (nome_disciplina) VALUES ('INFRAESTRUTURA');
-INSERT INTO TB_DISCIPLINA (nome_disciplina) VALUES ('ARQUITETURA');
-INSERT INTO TB_DISCIPLINA (nome_disciplina) VALUES ('ESTRUTURA METALICA');
-INSERT INTO TB_DISCIPLINA (nome_disciplina) VALUES ('CIVIL');
-INSERT INTO TB_DISCIPLINA (nome_disciplina) VALUES ('ELETRICA');
-INSERT INTO TB_DISCIPLINA (nome_disciplina) VALUES ('INSTRUMENTACAO-AUTOMACAO');
-INSERT INTO TB_DISCIPLINA (nome_disciplina) VALUES ('TELECOMUNICACAO');
-INSERT INTO TB_DISCIPLINA (nome_disciplina) VALUES ('SPCI');
-INSERT INTO TB_DISCIPLINA (nome_disciplina) VALUES ('ENGENHARIA DE CUSTO');
+INSERT INTO tb_disciplina (nome_disciplina) VALUES ('COORDENACAO');
+INSERT INTO tb_disciplina (nome_disciplina) VALUES ('PLANEJAMENTO');
+INSERT INTO tb_disciplina (nome_disciplina) VALUES ('ARQUIVO');
+INSERT INTO tb_disciplina (nome_disciplina) VALUES ('PROCESSO');
+INSERT INTO tb_disciplina (nome_disciplina) VALUES ('SISTEMAS');
+INSERT INTO tb_disciplina (nome_disciplina) VALUES ('TUBULACAO');
+INSERT INTO tb_disciplina (nome_disciplina) VALUES ('MECANICA');
+INSERT INTO tb_disciplina (nome_disciplina) VALUES ('INFRAESTRUTURA');
+INSERT INTO tb_disciplina (nome_disciplina) VALUES ('ARQUITETURA');
+INSERT INTO tb_disciplina (nome_disciplina) VALUES ('ESTRUTURA METALICA');
+INSERT INTO tb_disciplina (nome_disciplina) VALUES ('CIVIL');
+INSERT INTO tb_disciplina (nome_disciplina) VALUES ('ELETRICA');
+INSERT INTO tb_disciplina (nome_disciplina) VALUES ('INSTRUMENTACAO-AUTOMACAO');
+INSERT INTO tb_disciplina (nome_disciplina) VALUES ('TELECOMUNICACAO');
+INSERT INTO tb_disciplina (nome_disciplina) VALUES ('SPCI');
+INSERT INTO tb_disciplina (nome_disciplina) VALUES ('ENGENHARIA DE CUSTO');
 
 CREATE TABLE tb_formato (
   id_formato INTEGER(2) NOT NULL AUTO_INCREMENT UNIQUE,
@@ -34,12 +34,12 @@ CREATE TABLE tb_formato (
   PRIMARY KEY (id_formato)
 ) ENGINE=MyISAM;
 
-INSERT INTO TB_FORMATO (nome_formato) VALUES ('A0');
-INSERT INTO TB_FORMATO (nome_formato) VALUES ('10A4');
-INSERT INTO TB_FORMATO (nome_formato) VALUES ('A1');
-INSERT INTO TB_FORMATO (nome_formato) VALUES ('A2');
-INSERT INTO TB_FORMATO (nome_formato) VALUES ('A3');
-INSERT INTO TB_FORMATO (nome_formato) VALUES ('A4');
+INSERT INTO tb_formato (nome_formato) VALUES ('A0');
+INSERT INTO tb_formato (nome_formato) VALUES ('10A4');
+INSERT INTO tb_formato (nome_formato) VALUES ('A1');
+INSERT INTO tb_formato (nome_formato) VALUES ('A2');
+INSERT INTO tb_formato (nome_formato) VALUES ('A3');
+INSERT INTO tb_formato (nome_formato) VALUES ('A4');
 
 CREATE TABLE tb_projeto (
   id_projeto INTEGER(4) NOT NULL AUTO_INCREMENT UNIQUE,
@@ -60,18 +60,18 @@ CREATE TABLE tb_status (
   PRIMARY KEY (id_status)
 ) ENGINE=MyISAM;
 
-INSERT INTO TB_STATUS (nome_status) VALUE ('A PLANEJAR');
-INSERT INTO TB_STATUS (nome_status) VALUE ('A CONTRATAR');
-INSERT INTO TB_STATUS (nome_status) VALUE ('ADICIONAR NA LD');
-INSERT INTO TB_STATUS (nome_status) VALUE ('APROVADO');
-INSERT INTO TB_STATUS (nome_status) VALUE ('CANCELADO');
-INSERT INTO TB_STATUS (nome_status) VALUE ('EM ATENDIMENTO DE COMENTÁRIO');
-INSERT INTO TB_STATUS (nome_status) VALUE ('EM ELABORAÇÃO');
-INSERT INTO TB_STATUS (nome_status) VALUE ('EMITIDO');
-INSERT INTO TB_STATUS (nome_status) VALUE ('EMITIDO AC');
-INSERT INTO TB_STATUS (nome_status) VALUE ('EXCLUÍDO');
-INSERT INTO TB_STATUS (nome_status) VALUE ('PLANEJADO');
-INSERT INTO TB_STATUS (nome_status) VALUE ('VAGO');
+INSERT INTO tb_status (nome_status) VALUE ('A PLANEJAR');
+INSERT INTO tb_status (nome_status) VALUE ('A CONTRATAR');
+INSERT INTO tb_status (nome_status) VALUE ('ADICIONAR NA LD');
+INSERT INTO tb_status (nome_status) VALUE ('APROVADO');
+INSERT INTO tb_status (nome_status) VALUE ('CANCELADO');
+INSERT INTO tb_status (nome_status) VALUE ('EM ATENDIMENTO DE COMENTÁRIO');
+INSERT INTO tb_status (nome_status) VALUE ('EM ELABORAÇÃO');
+INSERT INTO tb_status (nome_status) VALUE ('EMITIDO');
+INSERT INTO tb_status (nome_status) VALUE ('EMITIDO AC');
+INSERT INTO tb_status (nome_status) VALUE ('EXCLUÍDO');
+INSERT INTO tb_status (nome_status) VALUE ('PLANEJADO');
+INSERT INTO tb_status (nome_status) VALUE ('VAGO');
 
 CREATE TABLE tb_tipo_documento (
   id_tipo_documento INTEGER(3) NOT NULL AUTO_INCREMENT UNIQUE,
@@ -79,52 +79,52 @@ CREATE TABLE tb_tipo_documento (
   PRIMARY KEY (id_tipo_documento)
 ) ENGINE=MyISAM;
 
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('ARRANJO');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('ARRANJO BASICO');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('ARRANJO GERAL');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('AS BUILT');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('CAPEX');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('CONJUNTO');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('CRITERIO DE PROJETO');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('DESENHO');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('DESENHO DE ARMACAO');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('DESENHO DE FORMA');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('DESENHO DE LOCACAO');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('DETALHE DE FABRICACAO');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('DETALHES TIPICOS');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('DIAGRAMA DE CONFIGURACAO');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('DIAGRAMA DE INTERLIGACAO');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('DIAGRAMA DE MONTAGEM');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('DIAGRAMA FUNCIONAL');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('DIAGRAMA TRIFILAR');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('DIAGRAMA UNIFILAR');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('ESPECIFICACAO DE SERVICO');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('ESPECIFICACAO TECNICA');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('ESTUDO DE DEMANDA');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('FLUXOGRAMA DE PROCESSO');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('FLUXOGRMA DE ENGENHARIA');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('FOLHA DE DADOS');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('LISTA DE CABOS');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('LISTA DE CHUMBADORES');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('LISTA DE EQUIPAMENTOS');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('LISTA DE EQUIPAMENTOS ELETRICOS');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('LISTA DE EQUIPAMENTOS TELECOMUNICACOES');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('LISTA DE INSTRUMENTOS');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('LISTA DE MATERIAIS');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('LISTA DE PARAFUSOS');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('LISTA DE SUPORTES');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('MAPA DE SUPRIMENTOS');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('MEMORIA DE CALCULO');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('MEMORIAL DESCRITIVO');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('MODELO 3D');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('PLANILHA DE QUANTIDADE');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('PLANO DE BASE COM CARGAS');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('PLANO DE CARGAS');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('PLANO DE SONDAGEM');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('PLANO DIRETOR');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('RELACAO E/S');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('RELATORIO');
-INSERT INTO TB_TIPO_DOCUMENTO (nome_tipo_documento) VALUES ('REQUISICAO TECNICA');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('ARRANJO');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('ARRANJO BASICO');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('ARRANJO GERAL');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('AS BUILT');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('CAPEX');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('CONJUNTO');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('CRITERIO DE PROJETO');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('DESENHO');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('DESENHO DE ARMACAO');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('DESENHO DE FORMA');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('DESENHO DE LOCACAO');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('DETALHE DE FABRICACAO');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('DETALHES TIPICOS');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('DIAGRAMA DE CONFIGURACAO');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('DIAGRAMA DE INTERLIGACAO');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('DIAGRAMA DE MONTAGEM');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('DIAGRAMA FUNCIONAL');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('DIAGRAMA TRIFILAR');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('DIAGRAMA UNIFILAR');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('ESPECIFICACAO DE SERVICO');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('ESPECIFICACAO TECNICA');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('ESTUDO DE DEMANDA');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('FLUXOGRAMA DE PROCESSO');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('FLUXOGRMA DE ENGENHARIA');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('FOLHA DE DADOS');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('LISTA DE CABOS');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('LISTA DE CHUMBADORES');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('LISTA DE EQUIPAMENTOS');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('LISTA DE EQUIPAMENTOS ELETRICOS');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('LISTA DE EQUIPAMENTOS TELECOMUNICACOES');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('LISTA DE INSTRUMENTOS');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('LISTA DE MATERIAIS');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('LISTA DE PARAFUSOS');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('LISTA DE SUPORTES');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('MAPA DE SUPRIMENTOS');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('MEMORIA DE CALCULO');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('MEMORIAL DESCRITIVO');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('MODELO 3D');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('PLANILHA DE QUANTIDADE');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('PLANO DE BASE COM CARGAS');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('PLANO DE CARGAS');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('PLANO DE SONDAGEM');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('PLANO DIRETOR');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('RELACAO E/S');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('RELATORIO');
+INSERT INTO tb_tipo_documento (nome_tipo_documento) VALUES ('REQUISICAO TECNICA');
 
 CREATE TABLE tb_controle (
   id_controle INTEGER(6) NOT NULL AUTO_INCREMENT UNIQUE,
